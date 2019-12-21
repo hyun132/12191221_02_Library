14dadd1daaf2de91624ab99968786937eaec34db
hyun <rlaalgus132@naver.com>
2019-12-21 12:43:40 +0900

    글라이드를 활용한 이미지뷰 이미지 처리

--- app/src/main/java/com/example/a12191221_02_library/MainActivity.kt
+++ app/src/main/java/com/example/a12191221_02_library/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.a12191221_02_library
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,9 +15,13 @@ class MainActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+
+
     }
 
     override fun setValues() {
+
+        Glide.with(mContext).load("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTSfMOs6lfIeOnK6hY4opBRyVa3LasUl_QpEOlcvlX4xDpcNhir").into(profileImgView)
 
     }
 
